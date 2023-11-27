519bb27004aaea23ab4c1118b3786ce8ae98f493
hashmi mohammed <hashmi.smf@gmail.com>
2023-11-28 02:35:14 +0530

    location state is used to communicate between the pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import ComponentA from './components/ComponentA';
-import { NavLink, Link, Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes, useLocation } from 'react-router-dom';
 import { NotFound } from './pages/NotFound';
 import { BookRoutes } from './pages/BookRoutes';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <nav>
         <ul>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/" state="Hello">Home</NavLink>
           </li>
           <li>
             <NavLink to="/books">Books</NavLink>
           </li>
         </ul>
       </nav>
+      {location.state}
       <Routes>
         <Route path="/" element={<ComponentA />} />
         <Route path="/books/*" element={<BookRoutes />} />
